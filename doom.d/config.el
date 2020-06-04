fd3b25119a3aa19c689399038106554f5fda9e12
Vlad Sterzhanov <gliderok@gmail.com>
2020-06-03 18:02:10 -0700

    move to doom

---
+++ doom.d/config.el
@@ -0,0 +1,92 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Vlad Sterzhanov"
+      user-mail-address "gliderok@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-gruvbox)
+; Favs: doom-gruvbox, doom-nord, tomorrow-day/night, spacegrey
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type `visual)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(setq org-roam-directory "~/Dropbox/org")
+
+(setq org-todo-keywords
+  '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+
+
+(map!
+ (:map general-override-mode-map
+  "s-l" #'evil-window-right
+  "s-h" #'evil-window-left
+  "s-j" #'evil-window-down
+  "s-k" #'evil-window-up
+  "s-L" #'evil-window-move-far-right
+  "s-H" #'evil-window-move-far-left
+  "s-J" #'evil-window-move-very-bottom
+  "s-K" #'evil-window-move-very-top))
+
+(map!
+ (:map evil-normal-state-map
+  "<s-return>" nil))
+
+(map!
+ (:after org
+ (:map org-mode-map
+  "<s-return>" #'org-todo)))
+
+(map!
+ (:map evil-motion-state-map
+  "C-o" nil
+  "C-i" nil))
+
+(map!
+ (:map evil-motion-state-map
+  "<C-o>" #'better-jumper-jump-forward
+  "C-o" #'better-jumper-jump-forward
+  "<C-i>" #'better-jumper-jump-backward
+  "C-i" #'better-jumper-jump-backward))
