5cec44b152a678803b09fed74866d9906e0959b5
Vlad Sterzhanov <gliderok@gmail.com>
2020-07-26 01:55:41 +0300

    add some favorite colorschemes

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ set -eux pipefail
 brew install macvim
 brew install autojump
 brew install jq
+brew cask install kitty
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo source ${DIR}/vlad_profile >> ~/.profile
@@ -13,6 +14,8 @@ ln -s ${DIR}/vlad_chunkwmrc ~/.chunkwmrc
 ln -s ${DIR}/vlad_skhdrc ~/.skhdrc
 ln -s ${DIR}/vlad_skhdrc_3 ~/.skhdrc_3
 ln -s ${DIR}/vlad_skhdrc_1 ~/.skhdrc_1
+rm ~/.config/kitty/kitty.conf
+ln -s ${DIR}/vlad_kitty.conf ~/.config/kitty/kitty.conf
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git config --global push.default current
