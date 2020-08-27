e02683f9394248039e038fe0aa4692f90b753253
Vlad Sterzhanov <gliderok@gmail.com>
2020-08-27 13:30:40 -0700

    add org-habit

--- doom.d/config.el
+++ doom.d/config.el
@@ -187,9 +187,15 @@
          ))
 )
 
+(use-package! org-habit)
+
+
+
 (use-package! org-agenda
   :init
   (setq org-agenda-block-separator nil
+        org-habit-show-habits t
+        org-habit-show-all-today t
         org-agenda-start-with-log-mode t
         org-agenda-log-mode-items '(closed clock state))
   (add-hook 'evil-org-agenda-mode-hook #'display-line-numbers-mode)
@@ -247,116 +253,6 @@
   (defun spolakh/set-todo-pass ()
     (interactive)
     (org-agenda-todo "PASS"))
-  ; This is heavily adopted from Sacha Chua's https://sachachua.com/blog/2013/01/emacs-org-display-projects-with-a-few-subtasks-in-the-agenda-view/
-  ;; (defun spolakh/org-agenda-project-agenda ()
-  ;;   "Return the project headline and up to `org-agenda-max-entries' tasks."
-  ;;   (save-excursion
-  ;;     (let* ((marker (org-agenda-new-marker))
-  ;;            (heading
-  ;;             (org-agenda-format-item "" (org-get-heading) (org-get-category) nil))
-  ;;            (org-agenda-restrict t)
-  ;;            (org-agenda-restrict-begin (point))
-  ;;            (org-agenda-restrict-end (org-end-of-subtree 'invisible))
-  ;;            ;; Find the TODO items in this subtree
-  ;;            (list (org-agenda-get-day-entries (buffer-file-name) (calendar-current-date) :todo)))
-  ;;       (org-add-props heading
-  ;;           (list 'face 'defaults
-  ;;                 'done-face 'org-agenda-done
-  ;;                 'undone-face 'default
-  ;;                 'mouse-face 'highlight
-  ;;                 'org-not-done-regexp org-not-done-regexp
-  ;;                 'org-todo-regexp org-todo-regexp
-  ;;                 'org-complex-heading-regexp org-complex-heading-regexp
-  ;;                 'help-echo
-  ;;                 (format "mouse-2 or RET jump to org file %s"
-  ;;                         (abbreviate-file-name
-  ;;                          (or (buffer-file-name (buffer-base-buffer))
-  ;;                              (buffer-name (buffer-base-buffer))))))
-  ;;         'org-marker marker
-  ;;         'org-hd-marker marker
-  ;;         'org-category (org-get-category)
-  ;;         'type "tagsmatch")
-  ;;       (concat heading "\n"
-  ;;               (org-agenda-finalize-entries list)))))
-  ;; (defun spolakh/org-agenda-projects-and-tasks (match)
-  ;;   "Show TODOs for all `org-agenda-files' headlines matching MATCH."
-  ;;   (interactive "MString: ")
-  ;;   (let ((todo-only nil))
-  ;;     (let* ((org-tags-match-list-sublevels
-  ;;             org-tags-match-list-sublevels)
-  ;;            (completion-ignore-case t)
-  ;;            rtn rtnall files file pos matcher
-  ;;            buffer)
-  ;;       (when (and (stringp match) (not (string-match "\\S-" match)))
-  ;;         (setq match nil))
-  ;;       (when match
-  ;;         (setq matcher (org-make-tags-matcher match)
-  ;;               match (car matcher) matcher (cdr matcher)))
-  ;;       (catch 'exit
-  ;;         (if org-agenda-sticky
-  ;;             (setq org-agenda-buffer-name
-  ;;                   (if (stringp match)
-  ;;                       (format "*Org Agenda(%s:%s)*"
-  ;;                               (or org-keys (or (and todo-only "M") "m")) match)
-  ;;                     (format "*Org Agenda(%s)*" (or (and todo-only "M") "m")))))
-  ;;         (org-agenda-prepare (concat "TAGS " match))
-  ;;         (org-compile-prefix-format 'tags)
-  ;;         (org-set-sorting-strategy 'tags)
-  ;;         (setq org-agenda-query-string match)
-  ;;         (setq org-agenda-redo-command
-  ;;               (list 'org-tags-view `(quote ,todo-only)
-  ;;                     (list 'if 'current-prefix-arg nil `(quote ,org-agenda-query-string))))
-  ;;         (setq files (org-agenda-files nil 'ifmode)
-  ;;               rtnall nil)
-  ;;         (while (setq file (pop files))
-  ;;           (catch 'nextfile
-  ;;             (org-check-agenda-file file)
-  ;;             (setq buffer (if (file-exists-p file)
-  ;;                              (org-get-agenda-file-buffer file)
-  ;;                            (error "No such file %s" file)))
-  ;;             (if (not buffer)
-  ;;                 ;; If file does not exist, error message to agenda
-  ;;                 (setq rtn (list
-  ;;                            (format "ORG-AGENDA-ERROR: No such org-file %s" file))
-  ;;                       rtnall (append rtnall rtn))
-  ;;               (with-current-buffer buffer
-  ;;                 (unless (derived-mode-p 'org-mode)
-  ;;                   (error "Agenda file %s is not in `org-mode'" file))
-  ;;                 (save-excursion
-  ;;                   (save-restriction
-  ;;                     (if org-agenda-restrict
-  ;;                         (narrow-to-region org-agenda-restrict-begin
-  ;;                                           org-agenda-restrict-end)
-  ;;                       (widen))
-  ;;                     (setq rtn (org-scan-tags 'spolakh/org-agenda-project-agenda matcher todo-only))
-  ;;                     (setq rtnall (append rtnall rtn))))))))
-  ;;         (if org-agenda-overriding-header
-  ;;             (insert (org-add-props (copy-sequence org-agenda-overriding-header)
-  ;;                         nil 'face 'org-agenda-structure) "\n")
-  ;;           (insert "Headlines with TAGS match: ")
-  ;;           (add-text-properties (point-min) (1- (point))
-  ;;                                (list 'face 'org-agenda-structure
-  ;;                                      'short-heading
-  ;;                                      (concat "Match: " match)))
-  ;;           (setq pos (point))
-  ;;           (insert match "\n")
-  ;;           (add-text-properties pos (1- (point)) (list 'face 'org-warning))
-  ;;           (setq pos (point))
-  ;;           (unless org-agenda-multi
-  ;;             (insert "Press `C-u r' to search again with new search string\n"))
-  ;;           (add-text-properties pos (1- (point)) (list 'face 'org-agenda-structure)))
-  ;;         (org-agenda-mark-header-line (point-min))
-  ;;         (when rtnall
-  ;;           (insert (mapconcat 'identity rtnall "\n") ""))
-  ;;         (goto-char (point-min))
-  ;;         (or org-agenda-multi (org-agenda-fit-window-to-buffer))
-  ;;         (add-text-properties (point-min) (point-max)
-  ;;                              `(org-agenda-type tags
-  ;;                                                org-last-args (,todo-only ,match)
-  ;;                                                org-redo-cmd ,org-agenda-redo-command
-  ;;                                                org-series-cmd ,org-cmd))
-  ;;         (org-agenda-finalize)
-  ;;         (setq buffer-read-only t)))))
   (defun spolakh/org-current-is-first-level-headline ()
     (= 1 (org-current-level)))
   (defun spolakh/org-current-is-todo ()
@@ -365,8 +261,6 @@
 	      (re-search-forward org-scheduled-time-regexp (line-end-position 2) t)
     )
 
-  ; org-with-limited-levels?
-  ; org-current-level
   (defun spolakh/org-agenda-leave-only-first-three-children ()
   "Returns each 1st level heading and at most 3 of it's TODO subheadings"
   (let (should-skip-entry)
@@ -411,7 +305,8 @@
             ((org-agenda-span 2)
              (org-agenda-start-day "today")
              (org-agenda-start-on-weekday nil)
-             (org-deadline-warning-days 14)))
+             (org-deadline-warning-days 14)
+             ))
     (todo "TODO"
           ((org-agenda-overriding-header "📤 To Refile >")
            (org-agenda-files '(,(concat spolakh/org-agenda-directory "inbox.org")
@@ -440,9 +335,6 @@
           ((org-agenda-overriding-header "🌒 Waiting >")
           (org-agenda-hide-tags-regexp "")
           ))
-    ;; (spolakh/org-agenda-projects-and-tasks "+PROJECT"
-    ;;  ((org-agenda-max-entries 3)
-    ;;   (org-agenda-files '(,(concat spolakh/org-agenda-directory "projects.org")))))
     (tags-todo ,(concat "TODO=\"TODO\"" filter)
           ((org-agenda-overriding-header "👾 One-off Tasks (under 1 Pomodoro) >")
           (org-agenda-files '(,(concat spolakh/org-agenda-directory "oneoff.org")))
