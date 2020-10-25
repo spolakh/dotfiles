309a5380536f02d817c179bd0579d035f97636ef
Vlad <vsterzhanov@grailbio.com>
2020-10-25 12:01:51 +0300

    make karabiner binding for :emacs actually work

--- karabiner/focus_emacs_and_spawn_if_needed.sh
+++ karabiner/focus_emacs_and_spawn_if_needed.sh
@@ -5,14 +5,17 @@ FRAME_NAME=$1
 
 
 spawn_new_frame_with_title () {
-    emacsclient -c -n -F "((name . \"${FRAME_NAME}\"))" --alternate-editor=""
+    echo "Spawning emacs with title ${FRAME_NAME}"
+    /usr/local/bin/emacsclient -c -n -F "((name . \"${FRAME_NAME}\"))" --alternate-editor=""
 }
 
 focus_emacs_window_with_title () {
   target_space=`/usr/local/bin/yabai -m query --windows | /usr/local/bin/jq "[.[] | select(.app | contains(\"Emacs\")) | select(.title | contains(\"$1\")) | .space][0]"`
   if [ $target_space == "null" ]; then
       target_space=0
+      echo "Emacs with title ${FRAME_NAME} not found"
   else
+    echo "Found Emacs with title ${FRAME_NAME} - focusing space #${target_space}"
     /usr/local/bin/yabai -m space --focus "${target_space}";
   fi
   return "${target_space}"
